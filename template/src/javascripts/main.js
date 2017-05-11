38a9d357743d5e7f0155829d67667c4c5c644c26
ziyi <ziyi.huang@ceasia.com.cn>
2017-05-11 21:03:23 +0800

    add app

--- template/src/javascripts/main.js
+++ template/src/javascripts/main.js
@@ -4,40 +4,13 @@ import React, { Component } from 'react';
 
 import ReactDOM from 'react-dom';
 
-import {
-  BrowserRouter as Router,
-  Route,
-  Link
-} from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-import Home from './components/Home';
-
-import About from './components/About';
+import App from './components/App.js';
 
 import '../stylesheets/style.css';
 
-class Main extends Component {
-  constructor(props){
-    super(props);
-  }
-  render(){
-    return (
-      <Router>
-        <div>
-        <h1>React Demo</h1>
-        <hr />
-        <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/about">About</Link></li>
-        </ul>
-        <hr />
-        <Route exact  path="/" component={Home} /> 
-        <Route path="/about" component={About} />
-        </div>
-      </Router>
-    );
-  }
-}
-
-
-ReactDOM.render(<Main />, document.getElementById('app'));
+ReactDOM.render(
+  <Router>
+    <App />
+  </Router>, document.getElementById('app'));
